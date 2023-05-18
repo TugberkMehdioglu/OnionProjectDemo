bec67002307209a3ed038fc90c4de84798e30423
Tuğberk <tugberkmehdioglu@gmail.com>
2023-05-18 12:45:40 +0300

    Created context class

---
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClasses
+{
+    public class MyContext : IdentityDbContext<AppUser, IdentityRole, string>
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.Load("Project.MAP"));
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<AppUser>? AppUsers { get; set; }
+        public DbSet<AppUserProfile>? AppUserProfiles { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Order>? Order { get; set; }
+        public DbSet<OrderDetail>? OrderDetail { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
