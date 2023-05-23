7a27ec6f10039776bf5289b51aff8792b4be4550
Tuğberk <tugberkmehdioglu@gmail.com>
2023-05-23 16:41:38 +0300

    Preparing UI

---
+++ Project.MVCUI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[Area]/[Controller]/[Action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Project.MVCUI/Controllers/HomeController.cs
+++ Project.MVCUI/Controllers/HomeController.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Project.MVCUI.Models;
 using System.Diagnostics;
 
 namespace Project.MVCUI.Controllers
 {
+    [Route("[Controller]/[Action]")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [Route("/")]
+        [Route("/Home")]
+        [Route("/Home/Index")]
         public IActionResult Index()
         {
             return View();
@@ -26,7 +22,7 @@ namespace Project.MVCUI.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
